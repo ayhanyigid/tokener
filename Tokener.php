89e4bead9680c8cee54f684ef7cecad40b310c0c
Ayhan YİĞİD <ayhanyigid@users.noreply.github.com>
2016-11-23 19:42:09 +0200

    Create Tokener.php

---
+++ Tokener.php
@@ -0,0 +1,29 @@
+<?php
+
+class Tokener
+{
+	private $key    = 'yusufkagan';
+	public  $string = 'www.yuhhu.net'; 
+
+	public function __construct()
+	{
+		if (empty($this->key)) {
+			throw new Exception("The key is empty");						
+		}
+	}
+
+	public function encrypt()
+	{
+		return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($this->key), $this->string, MCRYPT_MODE_CBC, md5(md5($this->key))));
+	}
+
+
+	public function decrypted($encrypted)
+	{
+		if (empty($encrypted)) {
+			throw new Exception("The encrypted key is empty");						
+		}
+
+		return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($this->key), base64_decode($encrypted), MCRYPT_MODE_CBC, md5(md5($this->key))), "\0");
+	}
+}
